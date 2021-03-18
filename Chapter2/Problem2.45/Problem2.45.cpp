025655345d1af3382648ee6d68845361ec223421
James Rosado <jarosado0911@gmail.com>
2021-03-18 06:48:45 -0400

    added two more programs

---
+++ Chapter2/Problem2.45/Problem2.45.cpp
@@ -0,0 +1,52 @@
+// Problem2.45.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <iomanip>
+
+using std::fixed;
+using std::setprecision;
+
+int main()
+{
+    int n,prod;
+
+    cout << "Please enter in a number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        prod = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            prod = prod * j;
+            if (j == i)
+            {
+                cout << j;
+            }
+            else
+            {
+                cout << j << '*';
+            }
+        }
+        cout << " = " << prod << endl;
+    }
+    return 0;
+}
+
+
